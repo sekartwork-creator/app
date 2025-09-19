653ec6e3a24d2cb9bce702fafcb031eb5a95660b
sekartwork-creator <sekartwork@gmail.com>
2025-09-19 11:15:32 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = "attendance-cache-v1";
+const CACHE_NAME = 'checkin-cache-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", event=>{
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event=>{
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response=>response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
