093e6fd9fb9beff7f95071f0def9d6e45b365673
sekartwork-creator <sekartwork@gmail.com>
2025-09-19 11:25:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,36 @@
-const CACHE_NAME = 'checkin-cache-v1';
+const CACHE_NAME = 'checkin-pwa-cache-v1';
 const urlsToCache = [
+  './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
 ];
 
+// ติดตั้ง service worker และ cache ไฟล์
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ดึงไฟล์จาก cache ก่อน แล้วค่อย fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// อัปเดต cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
